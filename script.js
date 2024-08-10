45edca6909936cbb1f0507510fe2d4842b0a77d0
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2024-08-10 13:19:12 +0100

    removing js comments

--- script.js
+++ script.js
@@ -21,36 +21,35 @@ let highScore = 0;
 let hasStarted = false;
 let stopSnake = false;
 
-// pushes possible x and y positions to seperate arrays
 for (i = 0; i <= canvas.width - cellSize; i += cellSize) {
     foodX.push(i);
     foodY.push(i);
 }
-// makes canvas interactive upon load
+
 canvas.setAttribute('tabindex', 1);
 canvas.style.outline = 'none';
 canvas.focus();
-// draws a square
+
 function drawSquare(x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, cellSize, cellSize);
 }
-// giving the food its coordinates
+
 function createFood() {
-    food.x = foodX[Math.floor(Math.random() * foodX.length)]; // random x position from array
-    food.y = foodY[Math.floor(Math.random() * foodY.length)]; // random y position from array
-    // looping through the snake and checking if there is a collision
+    food.x = foodX[Math.floor(Math.random() * foodX.length)];
+    food.y = foodY[Math.floor(Math.random() * foodY.length)];
+
     for (i = 0; i < snake.length; i++) {
         if (checkCollision(food.x, food.y, snake[i].x, snake[i].y)) {
             createFood();
         }
     }
 }
-// drawing food
+
 function drawFood() {
     drawSquare(food.x, food.y, foodColor);
 }
-// setting the colors for the canvas
+
 function setBackground(color1, color2) {
     ctx.fillStyle = color1;
     ctx.strokeStyle = color2;
@@ -67,7 +66,7 @@ function setBackground(color1, color2) {
     }
     ctx.stroke()
 }
-// creating the snake and pushing coordinates to the array
+
 function createSnake() {
     snake = [];
     for (let i = snakeLength; i > 0; i--) {
@@ -75,22 +74,22 @@ function createSnake() {
         snake.push({ x: k, y: 0 });
     }
 }
-// loops through the snake array and draws each element
+
 function drawSnake() {
     for (i = 0; i < snake.length; i++) {
         drawSquare(snake[i].x, snake[i].y, snakeColor);
     }
 }
-// keyboard interactions | direction != '...' doesn't let the snake go backwards
+
 function changeDirection(keycode) {
     if (keycode == 65 && direction != 'right') { directionQueue = 'left'; }
     else if (keycode == 87 && direction != 'down') { directionQueue = 'up'; }
     else if (keycode == 68 && direction != 'left') { directionQueue = 'right'; }
     else if (keycode == 83 && direction != 'up') { directionQueue = 'down' }
 }
-// changing the snake's movement
+
 function moveSnake() {
-    if(!stopSnake) {let x = snake[0].x; // getting the head coordinates
+    if(!stopSnake) {let x = snake[0].x;
     let y = snake[0].y;
 
     direction = directionQueue;
@@ -107,13 +106,12 @@ function moveSnake() {
     else if (direction == 'down') {
         y += cellSize;
     }
-    // removes the tail and makes it the new head
     let tail = snake.pop();
     tail.x = x;
     tail.y = y;
     snake.unshift(tail);}
 }
-// checks if too coordinates match up
+
 function checkCollision(x1, y1, x2, y2) {
     if (x1 == x2 && y1 == y2) {
         return true;
@@ -122,27 +120,24 @@ function checkCollision(x1, y1, x2, y2) {
         return false;
     }
 }
-// main game loop
+
 function game() {
     let head = snake[0];
-    // checking for wall collisions
+
     if (head.x < 0 || head.x > canvas.width - cellSize || head.y < 0 || head.y > canvas.height - cellSize) {
         openModal();
     }
-    // checking for body collisions 
     for (i = 1; i < snake.length; i++) {
         if (head.x == snake[i].x && head.y == snake[i].y) {
             openModal();
         }
     }
-    // checking for food collision 
     if (checkCollision(head.x, head.y, food.x, food.y)) {
         snake[snake.length] = { x: head.x, y: head.y };
         createFood();
         drawFood();
         score += 10;
     }
-
     window.onkeydown = function (evt) {
         evt = evt || window.event;
         changeDirection(evt.keyCode);
@@ -194,7 +189,6 @@ function checkScore(newScore) {
     }
 }
 
-// modals
 const modalStart = document.querySelector('.modal-start');
 const modalEnd = document.querySelector('.modal-end');
 const overlay = document.querySelector('.snake-overlay');
